1de7fb229b27290e07a5209ebde3c7d315950db0
JosephNK <nkw0608@gmail.com>
2024-09-13 21:47:53 +0900

    feat: working..

--- apps/inbound/src/inbound.module.ts
+++ apps/inbound/src/inbound.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { InboundController } from './inbound.controller';
 import { InboundService } from './inbound.service';
+import { validate } from '@app/common';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `apps/inbound/.env.${process.env.NODE_ENV}`,
+      validate,
+    }),
+  ],
   controllers: [InboundController],
   providers: [InboundService],
 })
-export class InboundModule {}
+export class InboundModule { }
--- apps/tanager/src/app.module.ts
+++ apps/tanager/src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { validate } from './env.validation';
+import { validate } from '@app/common';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `.env.${process.env.NODE_ENV}`,
+      envFilePath: `apps/tanager/.env.${process.env.NODE_ENV}`,
       validate,
     }),
   ],
